4bc07de35ab318e2807f6499ee2431fe5c66dc7b
Rinku Kumar <rinkukumar412616@gmail.com>
2024-07-19 08:29:38 +0530

    update

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -6,19 +6,20 @@ import { motion } from "framer-motion"
 
 const Photo = () => {
   return (
-    <div className="w-full h-full relative mix-blend-lighten">
+    <motion.div className="w-full h-full relative mix-blend-lighten">
       <motion.div
-      initial={{opacity:0}}
+      initial={{opacity:0, scale:0}}
       animate={{
-        opacity:1,
-        transition:{delay:2, duration:0.4, ease:"easeIn"}
+        opacity: 1,
+        scale: 1,
+        transition:{type:"spring", stiffness:120, damping:10, delay:1.4, duration:0.4, ease:"easeIn"}
       }}
       >
         <div className="h-[298px] w-[298px] xl:h-[498px] xl:w-[498px] relative">
           <Image src={`/luffy.png`} priority quality={100} fill alt="" sizes="auto" className="object-contain"></Image>
         </div>
       </motion.div>
-    </div>
+    </motion.div>
   )
 }
 
