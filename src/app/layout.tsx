b19adec3a9f0197232b1c27a3dfd63f0d14e9ce3
Rinku Kumar <rinkukumar412616@gmail.com>
2024-07-30 14:09:12 +0530

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono} from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // components
 import Header from "@/components/Header";
@@ -33,6 +34,7 @@ export default function RootLayout({
           <main>
             { children }
             <Analytics />
+            <SpeedInsights />
           </main>
       </body>
     </html>
