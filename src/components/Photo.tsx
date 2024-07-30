3e04599ae4927b6fa1bca66b8dfe22a5c8ea371c
Rinku Kumar <rinkukumar412616@gmail.com>
2024-07-30 15:48:45 +0530

    photo fix

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,27 +1,40 @@
-"use client";
-
-import Image from "next/image"
-import { motion } from "framer-motion"
-
+import Image from "next/image";
+// import { motion } from "framer-motion"
 
 const Photo = () => {
   return (
-    <motion.div className="w-full h-full relative mix-blend-lighten">
-      <motion.div
-      initial={{opacity:0, scale:0}}
-      animate={{
-        opacity: 1,
-        scale: 1,
-        transition:{type:"spring", stiffness:120, damping:10, delay:1, duration:0.4, ease:"easeIn"}
-      }}
+    <div className="w-full h-full relative mix-blend-lighten">
+      <div
+      // initial={{opacity:0, scale:0}}
+      // animate={{
+      //   opacity: 1,
+      //   scale: 1,
+      //   transition:{type:"spring", stiffness:120, damping:10, delay:1, duration:0.4, ease:"easeIn"}
+      // }}
       >
         <div className="min-h-[298px] min-w-[298px] xl:h-[498px] xl:w-[498px] relative">
-          <Image src={`/luffy.png`} alt="" priority quality={100} width={498} height={498} className="hidden xl:block xl:object-contain" />
-          <Image src={`/luffy.png`} alt="" priority quality={100} width={298} height={298} className="block xl:hidden object-contain" />
+          <Image
+            src={`/luffy.png`}
+            alt=""
+            priority
+            quality={100}
+            width={498}
+            height={498}
+            className="hidden xl:block xl:object-contain"
+          />
+          <Image
+            src={`/luffy.png`}
+            alt=""
+            priority
+            quality={100}
+            width={298}
+            height={298}
+            className="block xl:hidden object-contain"
+          />
         </div>
-      </motion.div>
-    </motion.div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Photo
+export default Photo;
