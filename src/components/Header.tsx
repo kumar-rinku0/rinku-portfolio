f149f271bc0cfabff66ba76d54b732c35eedfa5c
Rinku Kumar <rinkukumar412616@gmail.com>
2024-09-09 16:11:17 +0530

    cdn issue fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,35 @@
-import Link from "next/link"
-import { Button } from "./ui/button"
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 // components
-import Nav from "./Nav"
-import MobileNav from "./MobileNav"
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container px-4 flex xl:justify-evenly justify-between items-center">
-            {/* logo */}
-            <Link href="/">
-                <span className="text-2xl font-semibold">rinku <span className="text-accent">.</span></span>
-            </Link>
+      <div className="container px-4 flex xl:justify-evenly justify-between items-center">
+        {/* logo */}
+        <Link href="/" prefetch={false} scroll={false} replace={false}>
+          <span className="text-2xl font-semibold">
+            rinku <span className="text-accent">.</span>
+          </span>
+        </Link>
 
-            {/* desktop navbar */}
-            <div className="hidden lg:flex gap-8 items-center">
-                <Nav />
-                <Link href={'/contect'}>
-                    <Button> Hire me </Button>
-                </Link>
-            </div>
-            {/* mobile navbar */}
-            <div className="flex lg:hidden">
-                <MobileNav />
-            </div>
-
+        {/* desktop navbar */}
+        <div className="hidden lg:flex gap-8 items-center">
+          <Nav />
+          <Link href={"/contect"}>
+            <Button> Hire me </Button>
+          </Link>
         </div>
+        {/* mobile navbar */}
+        <div className="flex lg:hidden">
+          <MobileNav />
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { usePathname } from "next/navigation"
-import { Sheet, SheetContent, SheetTrigger, SheetClose } from "./ui/sheet"
-import { CiMenuFries } from "react-icons/ci"
-import Link from "next/link"
+import { usePathname } from "next/navigation";
+import { Sheet, SheetContent, SheetTrigger, SheetClose } from "./ui/sheet";
+import { CiMenuFries } from "react-icons/ci";
+import Link from "next/link";
 
 const links = [
   {
@@ -25,9 +25,8 @@ const links = [
   {
     name: "contect",
     path: "/contect",
-  }
-]
-
+  },
+];
 
 const MobileNav = () => {
   const pathname = usePathname();
@@ -39,29 +38,32 @@ const MobileNav = () => {
       <SheetContent>
         {/* logo */}
         <div className="mt-32 mb-40 text-2xl text-center font-semibold flex justify-center">
-          <Link href='/'>
-            <SheetClose >
+          <Link href="/" prefetch={false} scroll={false} replace={false}>
+            <SheetClose>
               rinku <span className="text-accent">.</span>
             </SheetClose>
           </Link>
         </div>
         {/* nav */}
-        <nav
-          className="flex flex-col justify-center items-center gap-8"
-        >{links.map((link, index) => {
-          return (
-            <Link
-              href={link.path}
-              key={index}
-              className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-base hover:text-accent`}
-            >
-              <SheetClose className="capitalize"> {link.name} </SheetClose>
-            </Link>
-          );
-        })}</nav>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } text-base hover:text-accent`}
+              >
+                <SheetClose className="capitalize"> {link.name} </SheetClose>
+              </Link>
+            );
+          })}
+        </nav>
       </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
 
-export default MobileNav 
+export default MobileNav;
